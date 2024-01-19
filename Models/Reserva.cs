66f2cba82a7e4d7b5d8bc84ee92b2d65f2d34cab
João Vitor <62443308+joaovianaAlves@users.noreply.github.com>
2024-01-19 13:05:21 -0300

    Add files via upload

---
+++ Models/Reserva.cs
@@ -0,0 +1,50 @@
+namespace DesafioProjetoHospedagem.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() { }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            if (hospedes.Count > 2)
+            {
+                throw new InvalidOperationException("Invalid number of guests.");
+            }
+            else
+            {
+                Hospedes = hospedes;
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            if (DiasReservados >= 10)
+            {
+                valor *= 0.90m;
+            }
+
+            return valor;
+        }
+    }
+}
